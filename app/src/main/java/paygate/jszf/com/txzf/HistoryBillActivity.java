3b09cca2f0185fc4dc1c2ec1edd3ddc8ec269d01
dhyking <779339753@qq.com>
2016-03-04 15:10:52 +0800

    Initial commit

---
+++ app/src/main/java/paygate/jszf/com/txzf/HistoryBillActivity.java
@@ -0,0 +1,13 @@
+package paygate.jszf.com.txzf;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HistoryBillActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_bill);
+    }
+}
